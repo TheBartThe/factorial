0c890c2e371434f5c9fd4c3e38850dc7acd89698
TheBartThe <vieran96@gmail.com>
2019-11-26 12:27:59 +0000

    first commit

---
+++ factorialTests/factorial_test.py
@@ -0,0 +1,14 @@
+import pytest
+from factorial import factorial
+
+def testFactorial1():
+    assert factorial.factorial(3628800) == 10
+
+def testFactorial2():
+    assert factorial.factorial(479001600) == 12
+
+def testFactorial3():
+    assert factorial.factorial(6) == 3
+
+def testFactorialNone():
+    assert factorial.factorial(18) == "NONE"
